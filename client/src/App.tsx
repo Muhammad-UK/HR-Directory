618d7dfc9ba32a53b3c9872173208a2cd7136bc2
Muhammad-UK <muhammadkhan3655@gmail.com>
2024-02-15 09:39:48 -0600

    started working on some frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,56 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
+type TEmployeeData = {
+  id?: number;
+  name: string;
+  department_id: number;
+};
+
 function App() {
-  return <></>;
+  const [employees, setEmployees] = useState<TEmployeeData[]>([]);
+
+  useEffect(() => {
+    const fetchEmployeeData = async () => {
+      try {
+        const response = await fetch("/api/employees");
+        const json = await response.json();
+        console.log(json);
+        setEmployees(json);
+      } catch (error) {}
+    };
+    fetchEmployeeData();
+  }, []);
+
+  return (
+    <div className="bg-base-300">
+      <h1 className="flex text-4xl justify-center px-4 py-16 bg-base-200">
+        HR Directory
+      </h1>
+      <div className="w-1/2 mx-auto">
+        <table className="table flex">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>Department ID</th>
+            </tr>
+          </thead>
+          <tbody>
+            {employees.map((employee, index) => {
+              return (
+                <tr>
+                  <th>{index++}</th>
+                  <td>{employee.name}</td>
+                  <td>{employee.department_id}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
 export default App;
